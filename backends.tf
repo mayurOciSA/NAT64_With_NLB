ee78557ca778fe995d563f747c400b95b677dfd9
Mayur Raleraskar <mayur.raleraskar@oracle.com>
2025-11-25 12:58:59 -0800

    adding variables for cidrs of ProxyVCN

--- backends.tf
+++ backends.tf
@@ -43,3 +43,20 @@ data "oci_core_vnic" "be_pvnic" {
   vnic_id = data.oci_core_vnic_attachments.be_pvnic_att[count.index].vnic_attachments[0].vnic_id
 }
 
+data "oci_core_private_ips" "backend_private_ipv4" {
+  subnet_id  = oci_core_subnet.backend_subnet.id
+  depends_on = [oci_core_instance.nat64_backend]
+}
+
+data "oci_core_ipv6s" "backend_private_ipv6" {
+  subnet_id  = oci_core_subnet.backend_subnet.id
+  depends_on = [oci_core_instance.nat64_backend]
+}
+
+output "backend_private_ipv6s" {
+  value = data.oci_core_ipv6s.backend_private_ipv6.ipv6s[*].ip_address
+}
+
+output "backend_private_ipv4s" {
+  value = data.oci_core_private_ips.backend_private_ipv4.private_ips[*].ip_address
+}
